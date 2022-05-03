adce21306854aae99862974c76abf81ba2b12711
Dae Glendowne <glendowne@code-intelligence.com>
2022-05-03 09:54:13 +0200

    initial commit

---
+++ src/main/java/app/ApplicationUnderTest.java
@@ -0,0 +1,30 @@
+/* Adapted from https://gist.github.com/jolor1/62e9e02e7fec4b6164fa9cb50a35dce9 */
+
+package app;
+
+import java.util.Base64;
+
+public class ApplicationUnderTest {
+  private static String base64Encode(String input) {
+    return Base64.getEncoder().encodeToString(input.getBytes());
+  }
+
+  // insecure encrypt with xor and static key
+  private static long insecureEncrypt(long input) {
+    long key = 0xefe4eb93215cb6b0L;
+    return input ^ key;
+  }
+
+  public static void consume(String str, long input1, long input2) {
+    if (base64Encode(str).equals("SmF6emVy")) { // base64Encode("Jazzer")
+      // To get here, the fuzzer has to guess the expected input byte by byte with value profile
+      if (insecureEncrypt(input1) == 0x9fc48ee64d3dc090L) { // insecureEncrypt(Long.toString("value"))
+        // To get here, the fuzzer has to guess the expected input bitwise with value profile
+        if (insecureEncrypt(input2) == 0x888a82ff483ad9c2L) { // insecureEncrypt(Long.toString("profile"))
+          //exception
+          int x = 5 / 0;
+        }
+      }
+    }
+  }
+}
